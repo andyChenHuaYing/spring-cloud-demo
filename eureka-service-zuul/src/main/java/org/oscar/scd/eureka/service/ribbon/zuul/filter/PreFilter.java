bedf84d517fff07e051a599c3d651b7fbb6d7a96
andyChenHuaYing <461857202@qq.com>
2018-08-29 23:00:58 +0800

    Finished zuul pre filter

---
+++ eureka-service-zuul/src/main/java/org/oscar/scd/eureka/service/ribbon/zuul/filter/PreFilter.java
@@ -0,0 +1,39 @@
+package org.oscar.scd.eureka.service.ribbon.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Service
+public class PreFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        return "yes".equals(request.getParameter("shouldFilter"));
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        log.info("Token:{}", token);
+        return null;
+    }
+}
