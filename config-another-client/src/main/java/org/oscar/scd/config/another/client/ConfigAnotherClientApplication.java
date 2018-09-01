1a82b49d3720f6fae574588e9c41ac3bec33077d
andyChenHuaYing <461857202@qq.com>
2018-09-01 12:18:40 +0800

    Finished spring-cloud-config

---
+++ config-another-client/src/main/java/org/oscar/scd/config/another/client/ConfigAnotherClientApplication.java
@@ -0,0 +1,25 @@
+package org.oscar.scd.config.another.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ConfigAnotherClientApplication {
+
+    @Value("${foo}")
+    private String foo;
+
+    @RequestMapping("/readFooProp")
+    public String readFooProp() {
+        return this.foo;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigAnotherClientApplication.class, args);
+    }
+}
